94445fc63b2afbbcbab10f74acd27e0d94827ba3
kellyrocha89 <132869637+kellyrocha89@users.noreply.github.com>
2023-05-08 01:50:00 -0300

    backend

---
+++ backend/Fornecedor.java
@@ -0,0 +1,69 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Fornecedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cnpjCpf;
+    private String nome;
+    private String email;
+    private String cep;
+
+    public Fornecedor() {
+        // Construtor vazio necessário para o mapeamento do JPA
+    }
+
+    public Fornecedor(String cnpjCpf, String nome, String email, String cep) {
+        this.cnpjCpf = cnpjCpf;
+        this.nome = nome;
+        this.email = email;
+        this.cep = cep;
+    }
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCnpjCpf() {
+        return cnpjCpf;
+    }
+
+    public void setCnpjCpf(String cnpjCpf) {
+        this.cnpjCpf = cnpjCpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
